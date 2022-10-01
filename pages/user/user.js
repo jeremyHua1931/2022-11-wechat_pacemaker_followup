6f56678fa54984d3adf6b0f07852368cc383e5f9
Hua Zhangzhao <jeremyhua@foxmail.com>
2022-10-01 21:48:26 +0800

    feature: 增加底部任务栏和个人信息页面(仿照ColorUI)
    具体写法见 /example_ColorUI
    
    Signed-off-by: Hua Zhangzhao <jeremyhua@foxmail.com>

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,66 +1,58 @@
-// pages/user/user.js
-Page({
-
-    /**
-     * 页面的初始数据
-     */
+Component({
+    options: {
+      addGlobalClass: true,
+    },
     data: {
-
+      starCount: 3,
+      forksCount: 23,
+      visitTotal: 4000,
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-
+    attached() {
+      console.log("success")
+      let that = this;
+      wx.showLoading({
+        title: '数据加载中',
+        mask: true,
+      })
+      let i = 0;
+      wx.hideLoading()
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+    methods: {
+      coutNum(e) {
+        // if (e > 1000 && e < 10000) {
+        //   e = (e / 1000).toFixed(1) + 'k'
+        // }
+        // if (e > 10000) {
+        //   e = (e / 10000).toFixed(1) + 'W'
+        // }
+        return e
+      },
+      CopyLink(e) {
+        wx.setClipboardData({
+          data: e.currentTarget.dataset.link,
+          success: res => {
+            wx.showToast({
+              title: '已复制',
+              duration: 1000,
+            })
+          }
+        })
+      },
+      showModal(e) {
+        this.setData({
+          modalName: e.currentTarget.dataset.target
+        })
+      },
+      hideModal(e) {
+        this.setData({
+          modalName: null
+        })
+      },
+      showQrcode() {
+        wx.previewImage({
+          urls: ['https://image.weilanwl.com/color2.0/zanCode.jpg'],
+          current: 'https://image.weilanwl.com/color2.0/zanCode.jpg' // 当前显示图片的http链接      
+        })
+      },
     }
-})
+  })
